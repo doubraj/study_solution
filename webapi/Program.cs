18a9036f9060300190c643fd15cb449e0cc0fc3d
Jan <triketful@gmail.com>
2021-08-17 11:16:23 +0200

    Database seed with 35 product rows added

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -3,17 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using webapi.Data;
 
 namespace webapi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            await MigrateDatabaseAndSeed(host);
+            host.Run();
+        }
+
+        public static async Task MigrateDatabaseAndSeed(IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                var dataContext = services.GetRequiredService<DataContext>();
+                dataContext.Database.Migrate();
+                await Seed.SeedDataIfEmpty(dataContext);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during database migration");
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
