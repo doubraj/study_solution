0b7fd4ad5f8366c2933f9c5c23ea4c258fbdfda7
Jan <triketful@gmail.com>
2021-08-18 10:49:50 +0200

    API partialy implemented

--- webapi/Controllers/ProductsController.cs
+++ webapi/Controllers/ProductsController.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using webapi.Data;
+using webapi.Models;
 
 namespace webapi.Controllers
 {
@@ -12,9 +17,33 @@ namespace webapi.Controllers
             _context = context;
         }
 
-        // TODO: Get all
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> getAllProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
         // TODO: Get all v2 (support for pagination)
         // TODO: Get one by ID
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> getProductById(Guid id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
         // TODO: Update one by ID
+        [HttpPut("{id}")]
+        public ActionResult<Product> updateProductDescriptionById(Guid id, Product product)
+        {
+            // Test implementation
+            // TODO: implement real logic
+            Console.WriteLine(product.Description);
+            return new Product
+            {
+                Id = new Guid(),
+                Name = "pocitac",
+                ImgUri = "asdfasdfasdf.jpg",
+                Price = 199,
+                Description = ""
+            };
+        }
     }
 }
